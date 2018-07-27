6b9cffebc23a4f5de8fe8f13f057079bd25b4d45
alexalsid <aalsid98@gmail.com>
2018-07-27 16:11:51 -0400

    challenge 2 completed

--- README.md
+++
@@ -1,6 +0,0 @@
-# IDM Git Practice
-
-Let's practice GIT together 😃.
-
-- [Challenge 1](Challenge_1.md)
-- [Challenge 2](Challenge_2.md)
--- challenge_2/ara/uglify.js
+++ challenge_2/ara/uglify.js
@@ -1,9 +1,17 @@
-var uglifycss = require('uglifycss');
+const uglifycss = require('uglifycss');
+const write = require('write');
+const fs = require('fs-extra');
 
-var uglified = uglifycss.processFiles(
-    ['src/styles.css'], { maxLineLen: 500, expandVars: true });
+
+
+
+const uglified = uglifycss.processFiles(
+    ['src/styles.css'], { maxLineLen: 500, expandVars: true,});
 
 console.log(uglified);
+
+
+write('dist/uglified.css', uglified);
 
 
 
